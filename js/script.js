f8cc7e9c4504afd3b5869551b3e2d965aa457eee
Teggour <TeggourMalik@gmail.com>
2021-05-27 18:42:07 +0300

    add HTML and CSS maket

--- js/script.js
+++ js/script.js
@@ -2,26 +2,30 @@ let cityName = "Zaporozhe",
     countryName = "UA",
     cityID = "697107";
 
-const keyAPI = "9487a2a81edbb899e539bf9c2b800a8f",
+const keyAPI = "9487a2a81edbb899e539bf9c2b800a8f", // from https://openweathermap.org/
     lang = "en";
 
+// convert kelvin to celsius
 function convertK_To_C(tempK) {
     return (Math.round(+tempK - 273.15))
 }
 
-function makeURL(id) {
+function createURLForIMG(id) {
     return (`https://openweathermap.org/img/wn/${id}@2x.png`)
 }
 
-function dateAndTime() {
-    let date = new Date(),
-        hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(),
-        minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes(),
-        dayOfWeek = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date),
-        month = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date),
-        numDayOfMonth = date.getDate()
-
-    return `${dayOfWeek}, ${numDayOfMonth} ${month}, ${hours}:${minutes}`
+function getDate() {
+    let date = new Date();
+    return (
+        {
+            weakDay: new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date),
+            minutes: new Intl.DateTimeFormat('en-US', { minute: 'numeric' }).format(date),
+            hours: new Intl.DateTimeFormat('en-US', { hour12: false, hour: 'numeric' }).format(date),
+            dayOfMonth: new Intl.DateTimeFormat('en-US', { day: 'numeric' }).format(date),
+            month: new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date),
+            year: new Intl.DateTimeFormat('en-US', { year: 'numeric' }).format(date)
+        }
+    )
 }
 
 fetch(`http://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${keyAPI}&lang=${lang}`)
@@ -30,12 +34,16 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${keyAP
     })
 
     .then((data) => {
-        let { name, main, weather, wind } = data;
-        let date = dateAndTime()
-        console.log(date)
+        let { name, main, weather, wind, sys } = data;
+        weather[0].iconURL = createURLForIMG(weather[0].icon)
 
-        weather[0].iconURL = makeURL(weather[0].icon)
-        console.log({ name, main, weather, wind })
+        let myDate = getDate()
 
-        
+        // setInterval(() => {
+        //     myDate = getDate()
+        //     document.querySelector(".weather-info").innerHTML = `${myDate.hours}:${myDate.minutes}`
+        // }, 1000)
+
+        console.log({ name, main, weather, wind, sys })
+        console.log(data)
     });
