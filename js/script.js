9033fee2773ee6cde53f9987be83cb6bd3e2ff65
Teggour <TeggourMalik@gmail.com>
2021-05-27 19:55:24 +0300

    add JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+let elementDateAndTime = document.querySelector(".weather-info__date-time"),
+    elementGeo = document.querySelector(".weather-info__geolocation"),
+    elementInfoIMG = document.querySelector(".weather-info__info-img"),
+    elementInfoTxt = document.querySelector(".weather-info__info-txt"),
+    elementDescr = document.querySelector(".weather-info__descr"),
+    elementWind = document.querySelector(".about-weather__wind"),
+    elementHumidity = document.querySelector(".about-weather__humidity"),
+    elementVisibility = document.querySelector(".about-weather__visibility"),
+    elementPressure = document.querySelector(".about-weather__pressure");
+
+// from "json/city.list.json"
 let cityName = "Zaporozhe",
     countryName = "UA",
     cityID = "697107";
@@ -28,22 +39,41 @@ function getDate() {
     )
 }
 
+function changeDOM(obj) { 
+    myDate = getDate()
+        elementDateAndTime.innerText = `${myDate.weakDay}, ${myDate.dayOfMonth} ${myDate.month}, ${myDate.hours}:${myDate.minutes}`
+
+    // date and time
+    setInterval(() => {
+        myDate = getDate()
+        elementDateAndTime.innerText = `${myDate.weakDay}, ${myDate.dayOfMonth} ${myDate.month}, ${myDate.hours}:${myDate.minutes}`
+    }, 1*1000)
+
+    //
+    elementGeo.innerText = `${obj.name}, ${obj.sys.country}`
+
+    elementInfoIMG.src = `${obj.weather[0].iconURL}`
+    elementInfoIMG.alt = `icon`
+    elementInfoTxt.innerText = `${convertK_To_C(obj.main.temp)}°`
+
+    elementDescr.innerText = `Feels like ${convertK_To_C(obj.main.feels_like)}°. ${obj.weather[0].main}. ${obj.weather[0].description}.`
+
+    elementWind.innerText = `Wind: ${obj.wind.speed}m/s`
+    elementVisibility.innerText = `Visibility: ${(obj.visibility/1000).toFixed(2)}km`
+    elementPressure.innerText = `Pressure: ${obj.main.pressure}hPa`
+    elementHumidity.innerText = `Humidity: ${obj.main.humidity}%`
+}
+
 fetch(`http://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${keyAPI}&lang=${lang}`)
     .then((response) => {
         return response.json();
     })
 
     .then((data) => {
-        let { name, main, weather, wind, sys } = data;
+        let { name, main, weather, wind, sys, visibility } = data;
         weather[0].iconURL = createURLForIMG(weather[0].icon)
 
         let myDate = getDate()
 
-        // setInterval(() => {
-        //     myDate = getDate()
-        //     document.querySelector(".weather-info").innerHTML = `${myDate.hours}:${myDate.minutes}`
-        // }, 1000)
-
-        console.log({ name, main, weather, wind, sys })
-        console.log(data)
+        changeDOM({ name, main, weather, wind, sys, visibility })
     });
